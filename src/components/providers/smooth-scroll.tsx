b777d2dbcdf5e920b98afdfe5efacf84604bb036
prathamesh <prathameshlonare9@gmail.com>
2026-08-11 01:07:01 +0530

    fix: remove lenis import from smooth-scroll (unused, breaks build)

--- src/components/providers/smooth-scroll.tsx
+++ src/components/providers/smooth-scroll.tsx
@@ -1,35 +1,7 @@
 "use client";
 
-import { ReactNode, useEffect } from "react";
-import Lenis from "lenis";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
+import { ReactNode } from "react";
 
 export function SmoothScroll({ children }: { children: ReactNode }) {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothWheel: true,
-      wheelMultiplier: 1,
-      touchMultiplier: 2,
-    });
-
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    lenis.on("scroll", ScrollTrigger.update);
-
-    requestAnimationFrame(raf);
-
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
-
   return <>{children}</>;
 }
